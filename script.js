69c8ea809fd94df24e0794516920f0066f94c99e
rakeshdubey4 <139318813+rakeshdubey4@users.noreply.github.com>
2023-08-12 15:13:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ function displayTime(){
     let hRotation = 30*hh + mm/2;
     let mRotation= 6*mm;
     let sRotation= 6*ss;
-hr.style.transform='rotate(${hRotation}deg)';
-min.style.transform='rotate(${mRotation}deg)';
-sec.style.transform='rotate(${sRotation}deg)';
+hr.style.transform=`rotate(${hRotation}deg)`;
+min.style.transform=`rotate(${mRotation}deg)`;
+sec.style.transform=`rotate(${sRotation}deg)`;
    
 }
 setInterval(displayTime, 1000);
